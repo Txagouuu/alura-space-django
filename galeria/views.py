7e4ac526021dd6f225ecf032668fc474d1246032
Txagouuu <tiagomendon45@gmail.com>
2025-12-10 15:06:30 -0300

    feat(views): integra página inicial com dados reais do banco (ORM)

--- galeria/views.py
+++ galeria/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-
+from galeria.models import fotografia
 
 def index(request):
 
-    dados = {
-    1:{"nome":"Nebulosa de Carina",
-       "legenda":"webbtelescope.org/ NASA /James Webb",},
-    2:{"nome":"Galáxia do NGC 1079",
-       "legenda":"nasa.org / NASA / Hubble",},
-
-}
-
-
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
