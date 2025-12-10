873d846a48f7bfc2ec8cb6727f7318ee85f7ba92
Tiago <tiagomendon45@gmail.com>
2025-12-10 16:55:26 -0300

    feat: registra model Fotografia e personaliza listagem no painel admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+ #classe para personalizar a exibição no admin
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)#necessario colocar uma viirgula no final para ser reconhecido como tupla
+    list_per_page = 10
+
+admin.site.register(Fotografia,ListandoFotografias)
